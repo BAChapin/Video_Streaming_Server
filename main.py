dd9370f85a6ccf52316b773f52a91a7c713b6d5c
Brett Chapin <brett.chapin89@gmail.com>
2023-10-31 23:15:16 -0400

    Test from PiCamera2 examples

--- main.py
+++ main.py
@@ -1,82 +1,96 @@
+#!/usr/bin/python3
+
+# Mostly copied from https://picamera.readthedocs.io/en/release-1.13/recipes2.html
+# Run this script, then point a web browser at http:<this-ip-address>:8000
+# Note: needs simplejpeg to be installed (pip3 install simplejpeg).
+
 import io
-from flask import Flask, Response
+import logging
+import socketserver
+from http import server
+from threading import Condition
+
 from picamera2 import Picamera2
-from picamera2.outputs import FileOutput
 from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
 
-app = Flask(__name__)
-camera = None
-streaming = False
-connection_count = 0  # Counter to monitor active connections
+PAGE = """\
+<html>
+<head>
+<title>picamera2 MJPEG streaming demo</title>
+</head>
+<body>
+<h1>Picamera2 MJPEG Streaming Demo</h1>
+<img src="stream.mjpg" width="640" height="480" />
+</body>
+</html>
+"""
 
-def generate_frames():
-    global camera, streaming
 
-    while streaming:
-        if camera is not None:
-            with io.BytesIO() as stream:
-                output = FileOutput(stream)
-                camera.start_recording(JpegEncoder(), output)
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
 
-                # for _ in camera.capture_continuous(stream, format="jpeg", use_video_port=True):
-                stream.seek(0)
-                yield (b"--frame\r\n"
-                        b"Content-Type: image/jpeg\r\n\r\n" + stream.read() + b"\r\n")
-                stream.seek(1)
-                stream.truncate()
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
 
-@app.route('/video')
-def video():
-    global camera, streaming, connection_count
 
-    if not streaming:
-        camera = Picamera2()
-        camera.configure(camera.create_video_configuration(main={"size": (640, 480)}))
-        streaming = True
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
 
-    connection_count += 1
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/stop')
-def stop():
-    global camera, streaming, connection_count
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
 
-    if streaming:
-        if camera is not None:
-            camera.stop()
-            camera.stop_encoder()
-        streaming = False
-    connection_count = 0
 
-    return "Video stream stopped."
+picam2 = Picamera2()
+picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+output = StreamingOutput()
+picam2.start_recording(JpegEncoder(), FileOutput(output))
 
-@app.route('/connections')
-def get_connection_count():
-    return f"Active Connections: {connection_count}"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
-
-# import cv2
-# import numpy
-# from flask import Flask, render_template, Response, stream_with_context, Request
-
-# video = cv2.VideoCapture(1)
-# app = Flask(__name__)
-
-# def video_stream():
-#     while(True):
-#         ret, frame = video.read()
-#         if not ret:
-#             break
-#         else:
-#             ret, buffer = cv2.imencode('.jpeg',frame)
-#             frame = buffer.tobytes()
-#             yield (b'--frame\r\n' b'Content-type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(video_stream(), mimetype= 'multipart/x-mixed-replace; boundary=frame')
-
-# if __name__ == "__main__":
-#     app.run(host ='0.0.0.0', port= '5000', debug=False)
+try:
+    address = ('', 8000)
+    server = StreamingServer(address, StreamingHandler)
+    server.serve_forever()
+finally:
+    picam2.stop_recording()
