2f0bbe09bd7d092b20bda91929b453555c1a437b
Brett Chapin <brett.chapin89@gmail.com>
2023-11-01 21:57:03 -0400

    More changes

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from time import sleep
 from picamera2 import Picamera2, Preview
 from picamera2.encoders import H264Encoder
 
@@ -8,3 +9,5 @@ video_config = picam2.create_video_configuration({"size": (1920, 1080)},
 picam2.configure(video_config)
 encoder = H264Encoder(40000000, False, 10)	# bitrate, repeat, iperiod
 picam2.start_recording(encoder, 'test.h264')
+sleep(5)
+picam2.stop_recording()
