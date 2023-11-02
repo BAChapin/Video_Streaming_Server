56bc12f66fd8477cf9d1ad67191f7f3f5d9fc11b
Brett Chapin <brett.chapin89@gmail.com>
2023-11-01 21:49:13 -0400

    Minor Fix

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from picamera2 import Picamera2, Preview
 from picamera2.encoders import H264Encoder
 
 picam2 = Picamera2()
-video_config = picam2.video_configuration({"size": (1920, 1080)},
+video_config = picam2.create_video_configuration({"size": (1920, 1080)},
                                           raw={"size": (1640, 1232)},
                                           controls={"FrameDurationLimits": (33333, 33333)})
 picam2.configure(video_config)
