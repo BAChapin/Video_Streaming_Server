193dca8a98b26c5409bed84650cad970b18e28be
Brett Chapin <brett.chapin89@gmail.com>
2023-11-03 00:46:02 -0400

    Added Flask functionality

--- live_streamer.py
+++ live_streamer.py
@@ -1,7 +1,10 @@
 import time
+from flask import Flask, Response
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
+
+app = Flask(__name__)
 
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
@@ -9,8 +12,24 @@ picam2.configure(video_config)
 
 encoder = H264Encoder(bitrate=16000, qp=30)
 
-output = FfmpegOutput("-f hls -hls_time 4 -hls_list_size 5 -hls_flags delete_segments -hls_allow_cache 0 stream.m3u8")
+output = FfmpegOutput("-f hls -hls_time 5 -hls_list_size 10 -hls_flags delete_segments -hls_allow_cache 5 stream.m3u8")
 
 picam2.start_recording(encoder, output)
-time.sleep(5)
-picam2.stop_recording()
+
+@app.route('/video')
+def video():
+    m3u8 = f'#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-TARGETDURATION:5\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:5,\n/video/0\n'
+    return Response(m3u8, content_type='Application/vnd.apple.mpegurl')
+
+@app.route('/video/<segment>')
+def video_segment(segment):
+    segment_number = int(segment)
+    with open(f'stream{segment_number}.ts', 'rb') as segment_file:
+        data = segment_file.read()
+    yield data
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+# time.sleep(5)
+# picam2.stop_recording()
