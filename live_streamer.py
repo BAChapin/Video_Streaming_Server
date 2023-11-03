a4675bb2345f5a667213e2f179969b8cc8e12081
Brett Chapin <brett.chapin89@gmail.com>
2023-11-03 00:32:08 -0400

    Added live_streamer.py

---
+++ live_streamer.py
@@ -0,0 +1,15 @@
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
+
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration()
+picam2.configure(video_config)
+
+encoder = H264Encoder(biterate=16000, qp=30)
+
+output = FfmpegOutput("-f hls -hls_time 4 -hls_list_size 5 -hls_flags delete_segments -hls_allow_cache 0 stream.m3u8")
+
+picam2.start_recording(encoder, output)
+time.sleep(5)
+picam2.stop_recording()
