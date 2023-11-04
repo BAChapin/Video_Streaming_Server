d7d9b73bfd11a28014e8c2b1cbfadc7e30c888db
Brett Chapin <brett.chapin89@gmail.com>
2023-11-03 21:55:25 -0400

    Next Test

--- live_streamer.py
+++ live_streamer.py
@@ -14,20 +14,20 @@ encoder = H264Encoder(bitrate=16000, qp=30)
 
 output = FfmpegOutput("-f hls -hls_time 5 -hls_list_size 10 -hls_flags delete_segments -hls_allow_cache 5 stream.m3u8")
 
-# @app.route('/video')
-# def video():
-#     m3u8 = f'#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-TARGETDURATION:5\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:5,\n/video/0\n'
-#     return Response(m3u8, content_type='Application/vnd.apple.mpegurl')
+@app.route('/video')
+def video():
+    m3u8 = f'#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-TARGETDURATION:5\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:5,\n/video/0\n'
+    return Response(m3u8, content_type='Application/vnd.apple.mpegurl')
 
-# @app.route('/video/<segment>')
-# def video_segment(segment):
-#     segment_number = int(segment)
-#     with open(f'stream{segment_number}.ts', 'rb') as segment_file:
-#         data = segment_file.read()
-#     yield data
+@app.route('/video/<segment>')
+def video_segment(segment):
+    segment_number = int(segment)
+    with open(f'stream{segment_number}.ts', 'rb') as segment_file:
+        data = segment_file.read()
+    yield data
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000, debug=True)
     picam2.start_recording(encoder, output)
-    time.sleep(25)
-    picam2.stop_recording()
+    # time.sleep(25)
+    # picam2.stop_recording()
+    app.run(host='0.0.0.0', port=5000, debug=True)
