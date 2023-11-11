161f71cff7ce0f19c8e1353f1261f0002c3f9424
Brett Chapin <brett.chapin89@gmail.com>
2023-11-11 13:34:44 -0500

    Test updates

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-from time import sleep
-from picamera2 import Picamera2, Preview
+import time
+from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
 
 picam2 = Picamera2()
-video_config = picam2.create_video_configuration({"size": (1920, 1080)},
-                                          raw={"size": (1640, 1232)},
-                                          controls={"FrameDurationLimits": (33333, 33333)})
+video_config = picam2.create_video_configuration(main={"size": (640, 480)})
 picam2.configure(video_config)
-encoder = H264Encoder(16000000, False, 10)	# bitrate, repeat, iperiod
-picam2.start_recording(encoder, 'test.mp4')
-sleep(5)
+
+encoder = H264Encoder(bitrate=16000, qp=30)
+
+output = FfmpegOutput("-f hls -hls_time 5 -hls_list_size 10 -hls_flags delete_segments -hls_allow_cache 5 stream.m3u8")
+
+picam2.start_recording(encoder, output)
+time.sleep(30)
 picam2.stop_recording()
