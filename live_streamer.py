7a46b7fbe0ec9308a5fc5f7fdc25e91b29fbd3ed
Brett Chapin <brett.chapin89@gmail.com>
2023-11-11 10:25:50 -0500

    Continue troubleshooting

--- live_streamer.py
+++ live_streamer.py
@@ -6,14 +6,6 @@ from picamera2.outputs import FfmpegOutput
 
 app = Flask(__name__)
 
-picam2 = Picamera2()
-video_config = picam2.create_video_configuration(main={"size": (640, 480)})
-picam2.configure(video_config)
-
-encoder = H264Encoder(bitrate=16000, qp=30)
-
-output = FfmpegOutput("-f hls -hls_time 5 -hls_list_size 10 -hls_flags delete_segments -hls_allow_cache 5 stream.m3u8")
-
 @app.route('/video')
 def video():
     m3u8 = f'#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-TARGETDURATION:5\n#EXT-X-MEDIA-SEQUENCE:0\n#EXTINF:5,\n/video/0\n'
@@ -27,8 +19,15 @@ def video_segment(segment):
     yield data
 
 if __name__ == '__main__':
+    picam2 = Picamera2()
     picam2.stop()
     picam2.close()
+    video_config = picam2.create_video_configuration(main={"size": (640, 480)})
+    picam2.configure(video_config)
+
+    encoder = H264Encoder(bitrate=16000, qp=30)
+
+    output = FfmpegOutput("-f hls -hls_time 5 -hls_list_size 10 -hls_flags delete_segments -hls_allow_cache 5 stream.m3u8")
     picam2.start_recording(encoder, output)
     # time.sleep(30)
     # picam2.stop_recording()
