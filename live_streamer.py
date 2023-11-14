41f8f9ad81b6fb8467ae0ac3b334afccf1d05bea
Brett Chapin <brett.chapin89@gmail.com>
2023-11-14 00:12:21 -0500

    Still testing where issue is

--- live_streamer.py
+++ live_streamer.py
@@ -12,15 +12,17 @@ picam2 = None
 
 def startCamera():
     picam2 = Picamera2()
-    picam2.start()
-    sleep(1)
 
     video_config = picam2.create_video_configuration(main={"size": (640, 480)})
     picam2.configure(video_config)
 
+    picam2.start()
+
     encoder = H264Encoder(bitrate=16000, qp=30)
 
     output = FfmpegOutput("-f hls -hls_time 5 -hls_list_size 10 -hls_flags delete_segments -hls_allow_cache 5 stream.m3u8")
+    sleep(1)
+    
     picam2.start_recording(encoder, output)
 
 @app.route('/video')
