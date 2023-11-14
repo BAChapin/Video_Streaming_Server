f2ef4a38d290ec9167b9fac553b1da59391cfbbe
Brett Chapin <brett.chapin89@gmail.com>
2023-11-14 00:06:17 -0500

    Put camera code in a thread

--- live_streamer.py
+++ live_streamer.py
@@ -1,11 +1,27 @@
 import time
 from time import sleep
+from threading import Thread
 from flask import Flask, Response
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
 
 app = Flask(__name__)
+
+picam2 = None
+
+def startCamera():
+    picam2 = Picamera2()
+    picam2.start()
+    sleep(1)
+
+    video_config = picam2.create_video_configuration(main={"size": (640, 480)})
+    picam2.configure(video_config)
+
+    encoder = H264Encoder(bitrate=16000, qp=30)
+
+    output = FfmpegOutput("-f hls -hls_time 5 -hls_list_size 10 -hls_flags delete_segments -hls_allow_cache 5 stream.m3u8")
+    picam2.start_recording(encoder, output)
 
 @app.route('/video')
 def video():
@@ -20,9 +36,9 @@ def video_segment(segment):
     yield data
 
 if __name__ == '__main__':
-    picam2 = Picamera2()
-    picam2.start()
-    sleep(1)
+    # picam2 = Picamera2()
+    # picam2.start()
+    # sleep(1)
 
     # video_config = picam2.create_video_configuration(main={"size": (640, 480)})
     # picam2.configure(video_config)
@@ -32,6 +48,9 @@ if __name__ == '__main__':
     # output = FfmpegOutput("-f hls -hls_time 5 -hls_list_size 10 -hls_flags delete_segments -hls_allow_cache 5 stream.m3u8")
     # picam2.start_recording(encoder, output)
 
+    streamThread = Thread(target=startCamera)
+    streamThread.start()
+    
     try:
         app.run(host='0.0.0.0', port=5000, debug=True)
     finally:
